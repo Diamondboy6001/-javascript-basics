37131abfa9bee72fc6b1b1754e805057b22c6ab7
Matthew Wilson <matthew281347@gmail.com>
2024-08-29 15:43:44 -0600

    i have added variables

---
+++ scripts/variables.js
@@ -0,0 +1,10 @@
+let intelligenceQuotient = 100;
+let firstName = "THURSDAY";
+let lastName = "thursday";
+console.log(firstName, lastName, "Has IQ", intelligenceQuotient);
+
+let mypet = {
+    type: "cat",
+    name: "lewis",
+    color: "white"
+}
