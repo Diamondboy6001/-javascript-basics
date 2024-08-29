f656b031d1ed0ebd75db4b76cfb41a7c84ba39b9
Matthew Wilson <matthew281347@gmail.com>
2024-08-29 16:17:48 -0600

    these are the functions

---
+++ scripts/functions.js
@@ -0,0 +1,13 @@
+//@ts-check
+
+let sayHi = function() {
+    let firstName = "Matthew"
+    let age = 14
+    let greeting =
+        "Hi i'm" + firstName + "and i am" + age + "years old"
+    console.log(greeting);
+}
+
+sayHi();
+
+console.log(firstName)
